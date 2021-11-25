4332e5a200acb74729c0f5ab301a94a8dcb52595
Donatas Tubutis <33428886+dktrdktr@users.noreply.github.com>
2021-11-25 16:30:50 -0500

    Exercises 4.8.-4.12.

--- part4/controllers/blogposts.js
+++ part4/controllers/blogposts.js
@@ -1,22 +1,19 @@
-const blogRouter = require('express').Router()
-const Blog = require('../models/blogpost')
+const blogRouter = require("express").Router();
+const Blog = require("../models/blogpost");
 
-blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogRouter.post("/", async (request, response) => {
+  const blog = await Blog(request.body);
+  if (blog.title === undefined || blog.url === undefined) {
+    response.status(400).end();
+  } else {
+    const result = await blog.save();
+    response.status(201).json(result);
+  }
+});
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-module.exports = blogRouter
+module.exports = blogRouter;
---
+++ part4/tests/bloglist_api.test.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blogpost");
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "Last Bicycle",
+    author: "Alain Dunkirk",
+    url: "www.google.lt",
+    likes: 12,
+  },
+  {
+    title: "First Drum",
+    author: "Timothy Leningrad",
+    url: "www.google.lt",
+    likes: 10,
+  },
+];
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
+test("there are two notes", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(2);
+});
+
+test("unique identifier property of the blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
+  response.body.map((item) => expect(item.id).toBeDefined());
+});
+
+test("POST: a valid blogpost can be added", async () => {
+  const newBlogpost = {
+    title: "Limits of Life",
+    author: "Moby Dick",
+    url: "www.wikipedia.com",
+    likes: 5,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlogpost)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  const contents = response.body.map((r) => r.title);
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  expect(contents).toContain("Limits of Life");
+});
+
+test("POST: likes defaults to 0", async () => {
+  const newPost = {
+    title: "Edites Thoughts",
+    author: "Oliver Lamarck",
+    url: "www.google.ru",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newPost)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blog = await Blog.find({ title: "Edites Thoughts" });
+
+  expect(blog[0].likes).toBe(0);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+test("POST: if the title and url are missing, the backend responds with 400 Bad Request", async () => {
+  const newPost = {
+    author: "Ronald Duck",
+  };
+
+  await api.post("/api/blogs").send(newPost).expect(400);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
