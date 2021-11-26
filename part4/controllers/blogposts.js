188a0fba41dcc075b42344260fe3bb18dc42f961
Donatas Tubutis <33428886+dktrdktr@users.noreply.github.com>
2021-11-25 20:10:26 -0500

    Exercises 4.13.-4.14.

--- part4/controllers/blogposts.js
+++ part4/controllers/blogposts.js
@@ -16,4 +16,25 @@ blogRouter.post("/", async (request, response) => {
   }
 });
 
+blogRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+
+  const blogpost = {
+    likes: body.likes,
+  };
+  const opts = { new: true, runValidators: true };
+
+  const updatedPerson = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blogpost,
+    opts
+  );
+  response.json(updatedPerson);
+});
+
 module.exports = blogRouter;
