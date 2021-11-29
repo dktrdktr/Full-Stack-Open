c2fd59849c66431978f1eb99412627f5acef4a1e
Donatas Tubutis <33428886+dktrdktr@users.noreply.github.com>
2021-11-29 14:33:45 -0500

    4.15: bloglist expansion, step3

--- part4/app.js
+++ part4/app.js
@@ -1,25 +1,28 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogposts')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogposts");
+const usersRouter = require("./controllers/users");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-const mongoUrl = config.MONGODB_URI
+const mongoUrl = config.MONGODB_URI;
 
-mongoose.connect(mongoUrl)
+mongoose
+  .connect(mongoUrl)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", usersRouter);
+app.use("/api/blogs", blogRouter);
 
-module.exports = app
+module.exports = app;
